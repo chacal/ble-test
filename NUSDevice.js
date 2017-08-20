ab69f57bf9ab2adff49ecbdd02cf0f3c053035db
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-08-20 14:19:21 +0300

    Promisify functions, get RX data as Bacon stream

--- NUSDevice.js
+++ NUSDevice.js
@@ -1,3 +1,5 @@
+const Bluebird = require('bluebird')
+const Bacon = require('baconjs')
 const NobleDevice = require('noble-device')
 const events = require('events')
 const util = require('util')
@@ -9,30 +11,26 @@ const NRFUART_TX_CHAR      = '6e400002b5a3f393e0a9e50e24dcca9e'
 
 const NUSDevice = function(peripheral) {
   NobleDevice.call(this, peripheral)
+  this.rx = Bacon.fromEvent(this, 'data')
 }
 
 util.inherits(NUSDevice, events.EventEmitter)
 NobleDevice.Util.inherits(NUSDevice, NobleDevice)
 NUSDevice.SCAN_UUIDS = [NRFUART_SERVICE_UUID]
 
+NUSDevice.discoverAsync = () => new Bluebird(NUSDevice.discover)
 
-NUSDevice.prototype.connectAndSetup = function(callback) {
-  const self = this
-
-  NobleDevice.prototype.connectAndSetup.call(self, function() {
-    self.notifyCharacteristic(NRFUART_SERVICE_UUID, NRFUART_RX_CHAR, true, data => self.emit("data", data), callback)
-  })
+NUSDevice.prototype.connectAndSetupAsync = function() {
+  return Bluebird.fromCallback(callback => this.connectAndSetup(() => {
+      this.notifyCharacteristic(NRFUART_SERVICE_UUID, NRFUART_RX_CHAR, true, data => this.emit("data", data), callback)
+    })
+  )
+  .then(() => this)
 }
 
-
-NUSDevice.prototype.write = function(data, done) {
-  if (typeof data === 'string') {
-    this.writeDataCharacteristic(NRFUART_SERVICE_UUID, NRFUART_TX_CHAR, new Buffer(data), done)
-  } else {
-    this.writeDataCharacteristic(NRFUART_SERVICE_UUID, NRFUART_TX_CHAR, data, done)
-  }
+NUSDevice.prototype.writeAsync = function(data) {
+  const buf = typeof data === 'string' ? new Buffer(data) : data
+  return Bluebird.fromCallback(callback => this.writeDataCharacteristic(NRFUART_SERVICE_UUID, NRFUART_TX_CHAR, buf, callback))
 }
-
-
 
 module.exports = NUSDevice
--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const NUSDevice = require('./NUSDevice')
 
 console.log('Scanning for devices..')
 
-NUSDevice.discover(function(nusDevice) {
-  console.log('Discovered NUS device:', nusDevice._peripheral.advertisement.localName)
-
-  nusDevice.on('disconnect', () => console.log('Disconnected!'))
-
-  nusDevice.connectAndSetup(function() {
-    nusDevice.readDeviceName(devName => console.log('Device name:', devName))
-
-    nusDevice.on('data', data => console.log('Received data:', data.toString()))
+NUSDevice.discoverAsync()
+  .tap(nusDevice => {
+    console.log('Discovered NUS device:', nusDevice._peripheral.advertisement.localName)
+    nusDevice.on('disconnect', () => console.log('Disconnected!'))
+  })
+  .then(nusDevice => nusDevice.connectAndSetupAsync())
+  .tap(() => console.log('Connected'))
+  .then(nusDevice => {
+    nusDevice.rx.onValue(data => console.log('Received data:', data.toString()))
 
     var writeCount = 0
     setInterval(() => {
       const TEST_DATA = 'Hello world! ' + writeCount++
-      nusDevice.write(TEST_DATA, () => console.log('data sent:', TEST_DATA))
+      nusDevice.writeAsync(TEST_DATA)
+        .then(() => console.log('data sent:', TEST_DATA))
     }, 3000)
   })
-})
+  .catch(e => console.log('ERROR', e))
